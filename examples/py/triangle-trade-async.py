8b8904605fcbf36af6fc0c7b2a6c9c669f0b0e78
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2021-02-14 17:04:30 +0000

    aycn triangle

--- examples/py/triangle-trade-async.py
+++ examples/py/triangle-trade-async.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import time
 import socket
 
 import ccxt.async_support as ccxta
@@ -25,6 +26,8 @@ default_mid_cur = 'USDT'
 delay = 2
 # 轮询订单次数query_times 3
 query_times = 3
+# 最小下单数量
+min_notional = 10 
 
 # good_exchange_name = ['binance', 'fcoin', 'gateio', 'huobipro', 'kucoin', 'okex']
 # good_exchange_name = ['binance', 'fcoin', 'gateio', 'huobipro', 'kucoin', 'okex','bcex','bibox','bigone','bitfinex','bitforex',
@@ -32,17 +35,15 @@ query_times = 3
 #                       'theocean','uex']
 # good_coin = ['ETH', 'XRP', 'BCH', 'EOS', 'XLM', 'LTC', 'ADA', 'XMR', 'TRX', 'BNB', 'ONT', 'NEO', 'DCR']
 
-good_exchange_name = ['uex']
-# good_coin = ['ETH', 'XRP', 'BCH', 'EOS', 'XLM', 'LTC', 'ADA', 'XMR', 'TRX', 'BNB', 'ONT', 'NEO', 'DCR', 'LBA', 'RATING']
-good_coin = ['ETH', 'XRP', 'BCH', 'EOS', 'RATING']
+good_exchange_name = ['binance']
+good_coin = ['ETH', 'XRP', 'BCH', 'EOS', 'XLM', 'LTC', 'ADA', 'XMR', 'TRX', 'BNB', 'ONT', 'NEO', 'DCR', 'RATING']
+#good_coin = ['BNB']
 
 
-has_config_exchange = ['uex']
+has_config_exchange = ['binance']
 config_key = dict()
-config_key['okex'] = ['okex_key','okex_secret']
-# config_key['uex'] = ['uex_key','uex_secret']
-config_key['uex'] = ['', '']
-
+#config_key['binance'] = ['CSTMpyrOapOukNAABAQMuX0pyr4CgRiKbbznMIKkLYvDf7E9U3ROqNQKzEdwoEde','uwMXqTayKskkSpQiA9NJA1V9vESNZj1jdBFrGFNd9aWYlhLmzv6F3C7B3jhl5wtP']
+config_key['binance'] = ['vaMCSPGdGTLdkcZKzyBtOkpGeVYPtb7F1OeI2gSPTHtjNh6Ze7qY7HLR2G2vmLte','sVpTMoJTZ0Bb21W73K6KpsNxt95MFn045V5jOjxVTQIyGM0zMeZnUkoiwlsuf7JT']
 # 交易相关常量
 # 订单交易量吃单比例
 order_ratio = 0.5
@@ -50,17 +51,19 @@ order_ratio = 0.5
 reserve_ratio_base = 0.3
 reserve_ratio_quote = 0.3
 reserve_ratio_mid = 0.3
+slippage_ratio = 0.0005
+service_ratio =0.0006
 
 # 最小成交量比例设定
 min_trade_percent = 0.2
 
-# 是否真正下单，默认否
+# 是否真正下单
 order_flag = False
 
 
 def set_proxy():
-    os.environ.setdefault('http_proxy', 'http://127.0.0.1:1080')
-    os.environ.setdefault('https_proxy', 'http://127.0.0.1:1080')
+    os.environ.setdefault('http_proxy', 'http://127.0.0.1:1087')
+    os.environ.setdefault('https_proxy', 'http://127.0.0.1:1087')
 
 
 # 获取指定交易所列表
@@ -69,6 +72,7 @@ def get_exchange_list(good_list):
     for exchange_name in good_list:
         exchange = getattr(ccxta,exchange_name)()
         if exchange:
+            #exchange.set_sandbox_mode(True)
             exchange_list.append(exchange)
     return exchange_list
 
@@ -85,9 +89,11 @@ def set_exchange_key(exchange):
 
 # 在指定交易所寻找三角套利机会，根据P3与P2/P1大小关系进行套利，暂不考虑滑点和手续费，目标保持base,quote数量不变，使mid数量增多
 async def find_trade_chance(exchange,base='LTC',quote='BTC',mid='USDT'):
-    print('-----find_trade_chance开始在交易所{}寻找三角套利机会,base:{},quote:{},mid:{}'.format(exchange.name,base,quote,mid))
+    #print('-----find_trade_chance开始在交易所{}寻找三角套利机会,base:{},quote:{},mid:{}'.format(exchange.name,base,quote,mid))
     try:
-        await exchange.load_markets()
+        markets =  await exchange.load_markets()
+        # for market in markets:
+        #     print('+++++++++++',market)
     except Exception as e:
         print('load_markets e is {} ,exchange is {}'.format(e.args[0],exchange.name))
         await exchange.close()
@@ -125,9 +131,7 @@ async def find_trade_chance(exchange,base='LTC',quote='BTC',mid='USDT'):
     size_base_quote_bid1 = book_base_quote['bids'][0][1] if len(book_base_quote['bids']) > 0 else None
     size_base_quote_ask1 = book_base_quote['asks'][0][1] if len(book_base_quote['asks']) > 0 else None
     date_time = exchange.last_response_headers['Date']
-    print('P1: buy1:{},{},sell1:{},{}'.format(price_quote_mid_bid1,size_quote_mid_bid1,price_quote_mid_ask1,size_quote_mid_ask1))
-    print('P2: buy1:{},{},sell1:{},{}'.format(price_base_mid_bid1, size_base_mid_bid1, price_base_mid_ask1,size_base_mid_ask1))
-    print('P3: buy1:{},{},sell1:{},{}'.format(price_base_quote_bid1, size_base_quote_bid1, price_base_quote_ask1,size_base_quote_ask1))
+
     #检查正循环套利
     '''
         三角套利的基本思路是，用两个市场（比如BTC/USDT，LTC/USDT）的价格（分别记为P1，P2），
@@ -139,46 +143,57 @@ async def find_trade_chance(exchange,base='LTC',quote='BTC',mid='USDT'):
         交易量Q3:三者中取最小下单量，单位要统一为P3交易对的个数
         利润：Q3*P1*(P2/P1-P3)
     '''
-    balance = await exchange.fetch_balance()
+    try:
+        balance = await exchange.fetch_balance()
+    except Exception as e:
+            print('-------find_trade_object fetch_balance exception is {}'.format(e.args[0]))
+            await exchange.close()
+            return 
     free_base = balance[base]['free'] if balance[base]['free'] else 0
     free_quote = balance[quote]['free'] if balance[quote]['free'] else 0
     free_mid = balance[mid]['free'] if balance[mid]['free'] else 0
-    if price_base_quote_ask1 < price_base_mid_bid1/price_quote_mid_ask1:
-        # trade_size = min(size_base_quote_ask1,size_base_mid_bid1,size_quote_mid_ask1/price_base_quote_ask1)
-        trade_size = get_buy_size(free_base, free_quote, free_mid, size_base_quote_ask1, size_quote_mid_ask1, 
+
+    trade_size = get_buy_size(free_base, free_quote, free_mid, size_base_quote_ask1, size_quote_mid_ask1, 
                                   price_base_quote_ask1, price_quote_mid_ask1)
+    if price_base_quote_ask1 < price_base_mid_bid1/price_quote_mid_ask1:
         price_diff = price_quote_mid_ask1*(price_base_mid_bid1/price_quote_mid_ask1 - price_base_quote_ask1)
         profit = trade_size*price_diff
-        print('++++++发现正套利机会 profit is {},price_diff is {},trade_size is {},P3: {} < P2/P1: {},time:{}\n\n'.format(
-            profit, price_diff, trade_size, price_base_quote_ask1, price_base_mid_bid1/price_quote_mid_ask1, date_time))
-        # 开始正循环套利
-        if order_flag:
-            postive_trade(exchange, cur_base_quote, cur_base_mid, cur_quote_mid, trade_size, price_base_quote_ask1,
-                          price_base_mid_bid1, price_quote_mid_ask1)
-        await exchange.close()
-        # 检查逆循环套利
-        '''
-            P3>P2/P1
-            操作：卖-买/卖
-            价格条件：base_quote_bid1买1 > base_mid_ask1卖1/quote_mid_bid1买1
-            交易量Q3:三者中取最小下单量
-            利润：Q3*P1*(P3-P2/P1)
-        '''
-    elif price_base_quote_bid1 > price_base_mid_ask1/price_quote_mid_bid1:
-        # trade_size = min(size_base_quote_bid1,size_base_mid_ask1,size_quote_mid_bid1/price_base_quote_bid1)
-        trade_size = get_sell_size(free_base, free_quote, free_mid, size_base_quote_bid1, size_base_mid_ask1, 
-                                   price_base_mid_ask1, price_base_quote_ask1)
+        if profit >= min_notional:
+            print('++++++发现正套利机会 profit is {} USDT,price_diff is {},trade_size is {},P3: {} < P2/P1: {},time:{}\n\n'.format(
+                profit, price_diff, trade_size, price_base_quote_ask1, price_base_mid_bid1/price_quote_mid_ask1, date_time))
+            print('P1: buy1:{},{},sell1:{},{}'.format(price_quote_mid_bid1,size_quote_mid_bid1,price_quote_mid_ask1,size_quote_mid_ask1))
+            print('P2: buy1:{},{},sell1:{},{}'.format(price_base_mid_bid1, size_base_mid_bid1, price_base_mid_ask1,size_base_mid_ask1))
+            print('P3: buy1:{},{},sell1:{},{}'.format(price_base_quote_bid1, size_base_quote_bid1, price_base_quote_ask1,size_base_quote_ask1))
+            # 开始正循环套利
+            if order_flag:
+                await postive_trade(exchange, cur_base_quote, cur_base_mid, cur_quote_mid, trade_size, price_base_quote_ask1,
+                            price_base_mid_bid1, price_quote_mid_ask1)
+            await exchange.close()
+            # 检查逆循环套利
+            '''
+                P3>P2/P1
+                操作：卖-买/卖
+                价格条件：base_quote_bid1买1 > base_mid_ask1卖1/quote_mid_bid1买1
+                交易量Q3:三者中取最小下单量
+                利润：Q3*P1*(P3-P2/P1)
+            '''
+    elif price_base_quote_bid1 > price_base_mid_ask1/price_quote_mid_bid1+service_charge+slippage:
+        trade_size = min(size_base_quote_bid1,size_base_mid_ask1,size_quote_mid_bid1/price_base_quote_bid1)
         price_diff = price_quote_mid_bid1*(price_base_quote_bid1-price_base_mid_ask1/price_quote_mid_bid1)
         profit = trade_size*price_diff
-        print('++++++发现逆套利机会 profit is {},price_diff is {},trade_size is {},P3: {} > P2/P1: {},time:{}\n\n'.format(
+        if profit >= min_notional:
+            print('P1: buy1:{},{},sell1:{},{}'.format(price_quote_mid_bid1,size_quote_mid_bid1,price_quote_mid_ask1,size_quote_mid_ask1))
+            print('P2: buy1:{},{},sell1:{},{}'.format(price_base_mid_bid1, size_base_mid_bid1, price_base_mid_ask1,size_base_mid_ask1))
+            print('P3: buy1:{},{},sell1:{},{}'.format(price_base_quote_bid1, size_base_quote_bid1, price_base_quote_ask1,size_base_quote_ask1))
+            print('++++++发现逆套利机会 profit is {},price_diff is {},trade_size is {},P3: {} > P2/P1: {},time:{}\n\n'.format(
             profit, price_diff, trade_size, price_base_quote_bid1, price_base_mid_ask1/price_quote_mid_bid1, date_time))
-        # 开始逆循环套利
-        if order_flag:
-            negative_trade(exchange, cur_base_quote, cur_base_mid, cur_quote_mid, trade_size, price_base_quote_bid1,
-                           price_base_mid_ask1, price_quote_mid_bid1)
-        await exchange.close()
+            # 开始逆循环套利
+            if order_flag:
+                await negative_trade(exchange, cur_base_quote, cur_base_mid, cur_quote_mid, trade_size, price_base_quote_bid1,
+                            price_base_mid_ask1, price_quote_mid_bid1)
+            await exchange.close()
     else:
-        print('在交易所{}没有找到三角套利机会,time:{}\n\n'.format(exchange.name,date_time))
+        #print('在交易所{}没有找到三角套利机会,time:{}\n\n'.format(exchange.name,date_time))
         await exchange.close()
 
 '''
@@ -222,9 +237,9 @@ async def postive_trade(exchange, base_quote, base_mid, quote_mid, trade_size, p
             retry += 1
             continue
         # 对冲卖P2 base_mid
-        hedge_sell(exchange, base_mid, filled, price_base_mid_bid1)
+        await hedge_sell(exchange, base_mid, filled, price_base_mid_bid1)
         # 对冲买P1 quote_mid
-        hedge_buy(exchange, quote_mid, filled, price_quote_mid_ask1)
+        await hedge_buy(exchange, quote_mid, filled, price_quote_mid_ask1)
 
         # 实际成交量完成目标，退出轮询
         if already_hedged_amount >= trade_size:
@@ -279,9 +294,9 @@ async def negative_trade(exchange, base_quote, base_mid, quote_mid, trade_size, 
             retry += 1
             continue
         # 对冲买LTC P2
-        hedge_buy(exchange, base_mid, filled, price_base_mid_ask1)
+        await hedge_buy(exchange, base_mid, filled, price_base_mid_ask1)
         # 对冲卖BTC P1
-        hedge_sell(exchange, quote_mid, filled, price_quote_mid_bid1)
+        await hedge_sell(exchange, quote_mid, filled, price_quote_mid_bid1)
         # 实际成交量完成目标，退出轮询
         if already_hedged_amount >= trade_size:
             print('逆向套利 negative_trade 实际成交量完成目标，退出轮询')
@@ -419,18 +434,22 @@ def get_host_ip():
     finally:
         s.close()
     return ip
-
-if __name__ == '__main__':
-    print('before proxy ip is {}'.format(get_host_ip()))
-    set_proxy()
-    print('after proxy ip is {}'.format(get_host_ip()))
-    good_exchange_list = get_exchange_list(good_exchange_name)
-    for exchange in good_exchange_list:
-        set_exchange_key(exchange)
-    # 在good_coin作为base，quote=BTC,mid=USDT 在good_exchange_list交易所列表中寻找套利机会
-    for symbol in good_coin:
+def tick():
+ for symbol in good_coin:
         for exchange in good_exchange_list:
             # find_trade_chance(exchange, symbol, default_quote_cur, default_mid_cur)
             asyncio.get_event_loop().run_until_complete(
                 find_trade_chance(exchange, symbol, default_quote_cur, default_mid_cur))
-
+if __name__ == '__main__':
+    # print('before proxy ip is {}'.format(get_host_ip()))
+    set_proxy()
+    # print('after proxy ip is {}'.format(get_host_ip()))
+    good_exchange_list = get_exchange_list(good_exchange_name)
+    for exchange in good_exchange_list:
+        set_exchange_key(exchange)
+    # 在good_coin作为base，quote=BTC,mid=USDT 在good_exchange_list交易所列表中寻找套利机会
+   
+    while True:
+        time.sleep(delay)
+        tick()
+ 
